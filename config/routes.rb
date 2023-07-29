3a668c433d58925a0893c2fb4ba304b2d8219ecb
Walter Alarcon Garces <walarcon@gmail.com>
2023-07-28 21:10:13 -0400

    se agregó la seccion "acerca de "

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'acerca/index'
+  
   
   get '/home', to: 'home#index'
+  get '/acerca', to: 'acerca#index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/acerca_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class AcercaControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get acerca_index_url
+    assert_response :success
+  end
+end
